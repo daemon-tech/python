73f548cc3358e60ab728cd960fdc58fc6f3147ae
0Day <59494058+le3ch-tech@users.noreply.github.com>
2021-01-08 13:14:32 +0100

    Add files via upload

---
+++ scripts/hackerrank.py
@@ -0,0 +1,19 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the superReducedString function below.
+def superReducedString(s):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = superReducedString(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
